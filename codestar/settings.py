eac5aec00e0fc355e3514cbd620d8b5dcd46786f
KernowPabloUK <PaulMorganUK1@outlook.com>
2025-06-27 12:44:01 +0100

    ADD: Add Collaborate Form into about section

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-    
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -30,7 +30,7 @@ SECRET_KEY = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
